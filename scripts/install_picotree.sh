19d2bf6c64ce2a6ca9349ad504c5108f527e850a
Miguel Yermo García <miguel.yermo@usc.es>
2026-02-07 20:49:39 +0100

    chore: improve picotree installation script

--- scripts/install_picotree.sh
+++ scripts/install_picotree.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+LIB_PREFIX="$(dirname "$(readlink -f "$0")")../lib"
 
 git clone -b v1.0.0 https://github.com/Jaybro/pico_tree.git picotree_source
 cd picotree_source
-cmake -B build -DBUILD_TESTING=0 -DBUILD_EXAMPLES=0 -DCMAKE_INSTALL_PREFIX=../picotree
+cmake -B build -DBUILD_TESTING=0 -DBUILD_EXAMPLES=0 -DCMAKE_INSTALL_PREFIX=${LIB_PREFIX}/picotree
 cmake --build build -- -j4
 cmake --install build
 cd ..
